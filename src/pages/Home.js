93189252b833cb856a8d07d2e69a13eb36dd2879
Asmaathabet <asmathabet2009@hotmail.com>
2021-02-09 16:53:57 +0200

    file stucture

---
+++ src/pages/Home.js
@@ -0,0 +1,6 @@
+const Home = () => {
+    return "here is home"
+}
+
+export default Home; 
+
---
+++ src/pages/Question.js
@@ -0,0 +1,6 @@
+const Question = () => {
+    return " here is Question"
+}
+
+export default Question; 
+
